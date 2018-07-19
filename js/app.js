30109f3427565562079bc3477978d39c96469f83
Stacy <smbridson@gmail.com>
2018-07-18 21:17:17 -0400

    added code to make cards dynamic

--- js/app.js
+++ js/app.js
@@ -27,9 +27,66 @@ function shuffle(array) {
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
+ * set up the event listener for a card. If a card is clicked:*/
+
+const deck = document.querySelector('.deck');
+
+let toggledCards = [];
+
+
+    deck.addEventListener('click', event => {
+        const clickTarget = event.target;
+        if (isClickValid(clickTarget)) {
+            toggleCard(clickTarget);
+            addToggleCard(clickTarget);
+            if (clickTarget.classList.contains('card') && toggledCards.length === 2){
+                checkForMatch(clickTarget);
+            }
+        }
+    });
+
+    function isClickValid(clickTarget) {
+        return (clickTarget.classList.contains('card') && 
+        !clickTarget.classList.contains('match') && toggledCards.length < 2 && !toggledCards.includes(clickTarget)
+        );
+    }
+
+    function toggleCard(card) {
+        card.classList.toggle('open');
+        card.classList.toggle('show');
+    }
+
+    function addToggleCard(clickTarget) {
+        toggledCards.push(clickTarget);
+        console.log(toggledCards);
+    }
+
+    function checkForMatch() {
+        if (toggledCards[0].firstElementChild.className === toggledCards[1].firstElementChild.className) {
+            toggledCards[0].classList.toggle('match');
+            toggledCards[1].classList.toggle('match');
+            toggledCards = [];
+        } else {
+            setTimeout(() => {
+                toggleCard(toggledCards[0]);
+                toggleCard(toggledCards[1]);
+                toggledCards = [];
+            }, 1000);
+        }
+    }
+
+
+
+ /*
+ * 
+ * 
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ * 
+ */
+
+
+/*
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
