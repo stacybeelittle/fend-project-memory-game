37496ca360b09f36a0a5d5e835167d3beabf6440
Stacy <smbridson@gmail.com>
2018-07-18 21:47:23 -0400

    shuffle deck function added

--- js/app.js
+++ js/app.js
@@ -31,6 +31,15 @@ function shuffle(array) {
 
 const deck = document.querySelector('.deck');
 
+function shuffleDeck() {
+    const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'));
+    const shuffledCards = shuffle(cardsToShuffle);
+    for (card of shuffledCards) {
+        deck.appendChild(card);
+    }
+}
+shuffleDeck();
+
 let toggledCards = [];
 
 
